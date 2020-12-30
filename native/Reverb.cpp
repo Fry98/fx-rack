865f448073083de069e94f78a6281ff4642d47ec
Vít Říha <vit.riha.97@gmail.com>
2020-12-30 21:32:45 +0100

    Reverb reverberates (finally)

--- native/Reverb.cpp
+++ native/Reverb.cpp
@@ -6,77 +6,66 @@
 
 namespace fx_rack {
   Reverb::Reverb(const pAudioFilter &child, double delay, double decay_factor, int mix_percent)
-    : AudioFilter(child), delay(delay), decay_factor(decay_factor), mix_percent(mix_percent) {
-      std::cout << "init" << std::endl;
+    : AudioFilter(child), delay(delay), decay_factor(decay_factor), mix_percent(mix_percent) {}
+
+  Reverb::~Reverb() {
+    if (buffer_initialzied) delete[] _buffer;
+  }
+
+  int Reverb::get_buffer_index(int current, int move_by) {
+    if (current < 0) return current + move_by;
+    int res = current + move_by;
+    while (res >= _buffer_size) {
+      res -= _buffer_size;
     }
+    while (res < 0) {
+      res += _buffer_size;
+    }
+    return res;
+  }
+
+  void Reverb::clear_buffer(int size) {
+    for (int i = 0; i < size; ++i) {
+      _buffer[get_buffer_index(_buffer_w, i)] = 0;
+    }
+  }
 
   error_type_t Reverb::do_process(audio_buffer_t &buffer) {
     if (buffer.valid_samples == 0) {
       return error_type_t::ok;
     }
-
-    std::vector<audio_sample_t> result = comb(buffer, 0, 0);
-    std::vector<audio_sample_t> comb_1 = comb(buffer, -11.73, -0.1313);
-    std::vector<audio_sample_t> comb_2 = comb(buffer, 19.31, -0.2743);
-    std::vector<audio_sample_t> comb_3 = comb(buffer, -7.97, -0.31);
-
-    for (int i = 0; i < buffer.data.size(); ++i) {
-      result[i] += comb_1[i] + comb_2[i] + comb_3[i];
+    if (!buffer_initialzied) {
+      int buffer_size_ms = delay + 19.31 + 11.73;
+      _buffer_size = 1.1 * buffer_size_ms * samp_freq / 1000.0;
+      _buffer_r = - (delay + 11.73) * samp_freq / 1000;
+      _buffer = new audio_sample_t[_buffer_size];
+      for (int i = 0; i < _buffer_size; ++i) {
+        _buffer[i] = 0;
+      }
+      buffer_initialzied = true;
     }
 
+    clear_buffer(buffer.data.size());
+    comb(buffer.data, 0, 0);
+    comb(buffer.data, -11.73, -0.1313);
+    comb(buffer.data, 19.31, -0.2743);
+    comb(buffer.data, -7.97, -0.31);
+
     for (int i = 0; i < buffer.data.size(); ++i) {
-      result[i] = (1.0 - mix_percent/100.0) * buffer.data[i] + mix_percent/100.0 * result[i];
+      if (_buffer_r + i >= 0) {
+        buffer.data[i] = (1.0 - mix_percent/100.0) * buffer.data[i] + mix_percent/100.0 * _buffer[get_buffer_index(_buffer_r, i)];
+      }
     }
 
-    result = all_pass(result);
-    result = all_pass(result);
-
-    buffer.data = result;
+    _buffer_w = get_buffer_index(_buffer_w, buffer.data.size());
+    _buffer_r = get_buffer_index(_buffer_r, buffer.data.size());
     return error_type_t::ok;
   }
 
-  std::vector<audio_sample_t> Reverb::comb(audio_buffer_t &buffer, double delay_diff, double decay_factor_diff) {
-    int delay_samples = (delay + delay_diff) * samp_freq / 1000000.0;
-    std::vector<audio_sample_t> comb_samples(buffer.data);
-    for (int i = 0; i < comb_samples.size() - delay_samples; ++i) {
-      comb_samples[i + delay_samples] += comb_samples[i] * (decay_factor + decay_factor_diff);
+  void Reverb::comb(std::vector<audio_sample_t> &samples, double delay_diff, double decay_factor_diff) {
+    int delay_samples = delay_diff * samp_freq / 1000;
+    for (int i = 0; i < samples.size(); ++i) {
+      _buffer[get_buffer_index(_buffer_w, delay_samples + i)] += samples[i] * (decay_factor + decay_factor_diff);
     }
-    return comb_samples;
-  }
-
-  std::vector<audio_sample_t> Reverb::all_pass(std::vector<audio_sample_t> &samples) {
-    int delay_samples = 89.27 * samp_freq / 1000000.0;
-    double decay_factor_ = 0.131;
-
-    std::vector<audio_sample_t> all_pass_samples(samples);
-    for (int i = 0; i < all_pass_samples.size(); ++i) {
-      if (delay_samples >= 0 && i >= delay_samples) {
-        all_pass_samples[i] += -decay_factor_ * all_pass_samples[i - delay_samples];
-      }
-      if (delay_samples >= 1 && i + 20 >= delay_samples && i + 20 - delay_samples < all_pass_samples.size()) {
-        all_pass_samples[i] += decay_factor_ * all_pass_samples[i + 20 - delay_samples];
-      }
-    }
-
-    audio_sample_t value = all_pass_samples[0];
-    audio_sample_t max = audio_sample_t(0, 0);
-    
-    for (int i = 0; i < all_pass_samples.size(); ++i) {
-      if (std::abs(all_pass_samples[i].left) > max.left || std::abs(all_pass_samples[i].right) > max.right) {
-        max = audio_sample_t(std::abs(all_pass_samples[i].left), std::abs(all_pass_samples[i].right));
-      }
-    }
-
-    // TODO fix clipping below
-    if (max.left > 0 && max.right > 0) {
-      for (int i = 0; i < all_pass_samples.size(); ++i) {
-        value.left = (value.left + (all_pass_samples[i].left - value.left)) / max.left;
-        value.right = (value.right + (all_pass_samples[i].right - value.right)) / max.right;
-        all_pass_samples[i] = audio_sample_t(value);
-        std::cout << value.left << ":" << value.right << std::endl;
-      }
-    }
-
-    return all_pass_samples;
   }
 }
--- native/Reverb.h
+++ native/Reverb.h
@@ -12,15 +12,22 @@ namespace fx_rack {
   class Reverb : public AudioFilter {
   public:
     Reverb(const pAudioFilter &child, double delay, double decay_factor, int mix_percent);
+    ~Reverb();
     error_type_t Reverb::do_process(audio_buffer_t &buffer);
   private:
     double delay;
     double decay_factor;
     int mix_percent;
     double samp_freq = convert_rate_to_int(get_params().rate);
-    audio_sample_t* buffer;
+    int _buffer_size;
+    audio_sample_t* _buffer;
+    int _buffer_w = 0;
+    int _buffer_r;
+    bool buffer_initialzied = false;
 
-    std::vector<audio_sample_t> Reverb::comb(audio_buffer_t &buffer, double delay_diff, double decay_factor_diff);
-    std::vector<audio_sample_t> Reverb::all_pass(std::vector<audio_sample_t> &samples);
+    void Reverb::comb(std::vector<audio_sample_t> &samples, double delay_diff, double decay_factor_diff);
+    void Reverb::all_pass(std::vector<audio_sample_t> &samples);
+    int Reverb::get_buffer_index(int current, int move_by);
+    void Reverb::clear_buffer(int size);
   };
 }
