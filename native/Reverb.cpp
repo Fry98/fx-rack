f272c8a981b9d59521c9fa5a32dd6bde85bc1fd0
Vít Říha <vit.riha.97@gmail.com>
2020-12-31 16:25:57 +0100

    Improve reverb algo - add all-pass filters

--- native/Reverb.cpp
+++ native/Reverb.cpp
@@ -24,7 +24,7 @@ namespace fx_rack {
     return res;
   }
 
-  void Reverb::clear_buffer(int size) {
+  void Reverb::clear_buffer(size_t size) {
     for (int i = 0; i < size; ++i) {
       _buffer[get_buffer_index(_buffer_w, i)] = 0;
     }
@@ -35,39 +35,59 @@ namespace fx_rack {
       return error_type_t::ok;
     }
     if (!buffer_initialzied) {
-      int buffer_size_ms = delay + 19.31 + 11.73;
-      _buffer_size = 1.1 * buffer_size_ms * samp_freq / 1000.0 + 1;
-      _buffer_r = - (delay + 11.73) * samp_freq / 1000;
+      buffer_initialzied = true;
+      int buffer_size_ms = (int) (delay + 19.31 + 11.73 + 89.27);
+      _buffer_size =(int) (2 * buffer_size_ms * samp_freq / 1000.0 + 1);
+      _buffer_r = (int) (-(delay + 11.73) * samp_freq / 1000);
+      _buffer_r_all_pass = (int) (_buffer_r - 89.27 * samp_freq / 1000);
       _buffer = new audio_sample_t[_buffer_size];
       for (int i = 0; i < _buffer_size; ++i) {
-        _buffer[i] = 0;
+        _buffer[i] = audio_sample_t();
       }
-      buffer_initialzied = true;
     }
 
-    clear_buffer(buffer.data.size());
+    clear_buffer(buffer.valid_samples);
     comb(buffer.data, 0, 0);
     comb(buffer.data, -11.73, -0.1313);
     comb(buffer.data, 19.31, -0.2743);
     comb(buffer.data, -7.97, -0.31);
 
-    for (int i = 0; i < buffer.data.size(); ++i) {
+    for (int i = 0; i < buffer.valid_samples; ++i) {
       if (_buffer_r + i >= 0) {
-        buffer.data[i] = (1.0 - mix_percent/100.0) * buffer.data[i] + mix_percent/100.0 * _buffer[get_buffer_index(_buffer_r, i)];
-      } else {
-        buffer.data[i] = (1.0 - mix_percent/100.0) * buffer.data[i];
+        _buffer[get_buffer_index(_buffer_r, i)] = (1.0 - mix_percent/100.0) * buffer.data[i] + mix_percent/100.0 * _buffer[get_buffer_index(_buffer_r, i)];
       }
     }
 
-    _buffer_w = get_buffer_index(_buffer_w, buffer.data.size());
-    _buffer_r = get_buffer_index(_buffer_r, buffer.data.size());
+    all_pass(buffer.data);
+    all_pass(buffer.data);
+
+    for (int i = 0; i < buffer.valid_samples; ++i) {
+      if (get_buffer_index(_buffer_r, i) > 0)
+        buffer.data[i] = _buffer[get_buffer_index(_buffer_r, i)];
+    }
+
+    _buffer_w = get_buffer_index(_buffer_w, (int) buffer.valid_samples);
+    _buffer_r = get_buffer_index(_buffer_r, (int) buffer.valid_samples);
+    _buffer_r_all_pass = get_buffer_index(_buffer_r_all_pass, (int)buffer.valid_samples);
     return error_type_t::ok;
   }
 
   void Reverb::comb(std::vector<audio_sample_t> &samples, double delay_diff, double decay_factor_diff) {
-    int delay_samples = delay_diff * samp_freq / 1000;
+    int delay_samples = (int) (delay_diff * samp_freq / 1000);
     for (int i = 0; i < samples.size(); ++i) {
       _buffer[get_buffer_index(_buffer_w, delay_samples + i)] += samples[i] * (decay_factor + decay_factor_diff);
+    }
+  }
+
+  void Reverb::all_pass(std::vector<audio_sample_t> &samples) {
+    if (_buffer_r < 0) return;
+    double decay_factor_ = 0.131;
+
+    for (int i = 0; i < samples.size(); ++i) {
+      if (get_buffer_index(_buffer_r_all_pass, i) >= 0) {
+        _buffer[get_buffer_index(_buffer_r, i)] += - decay_factor_ * _buffer[get_buffer_index(_buffer_r_all_pass, i)];
+        _buffer[get_buffer_index(_buffer_r, i)] += decay_factor_ * _buffer[get_buffer_index(_buffer_r_all_pass, i + 20)];
+      }
     }
   }
 }
